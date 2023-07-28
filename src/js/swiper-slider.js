bdf07144eab6c25fb2d4fdd89ea52fbcf47ccc48
roLLlyTHuK <71319633+roLLlyTHuK@users.noreply.github.com>
2023-07-28 12:36:10 +0300

    add light style text

--- src/js/swiper-slider.js
+++ src/js/swiper-slider.js
@@ -1,34 +1,35 @@
 const swiper = new Swiper('.swiper', {
   
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 16,
   // Responsive breakpoints
   breakpoints: {
     0: {
       slidesPerView: 1,
-      spaceBetween: 2
+      spaceBetween: 1
     },
     // when window width is >= 375px
     375: {
       slidesPerView: 1,
-      spaceBetween: 4
+      spaceBetween: 1
     },
     // when window width is >= 768px
     768: {
       slidesPerView: 2,
-      spaceBetween: 6
+      spaceBetween:3
     },
     // when window width is >= 1280px
     1280: {
       slidesPerView: 3,
-      spaceBetween: 16
+      spaceBetween: 8
     }
   },
   
-  centeredSlides: true,
+  centeredSlides: false,
   // Optional parameters
   direction: 'horizontal',
   loop: true,
+  autoHeight: true,
 
   // If we need pagination
   pagination: {
